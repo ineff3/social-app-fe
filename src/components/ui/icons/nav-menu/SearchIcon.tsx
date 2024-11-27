dd094814f361dc44f42c1709fa54a9c87ffed0b0
ineff3 <maksymbochkarov@gmail.com>
2024-11-27 16:02:20 +0100

    feat: add nav-menu selection transition.

---
+++ src/components/ui/icons/nav-menu/SearchIcon.tsx
@@ -0,0 +1,14 @@
+import { SVGProps } from 'react'
+
+export const SearchIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    viewBox="0 0 15 16"
+    fill="none"
+    stroke="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path d="M7.32819 13.3187C10.4434 13.3187 12.9688 10.7933 12.9688 7.6781C12.9688 4.56287 10.4434 2.03748 7.32819 2.03748C4.21295 2.03748 1.68756 4.56287 1.68756 7.6781C1.68756 10.7933 4.21295 13.3187 7.32819 13.3187Z" />
+    <path d="M13.5625 13.9125L12.375 12.725" />
+  </svg>
+)
